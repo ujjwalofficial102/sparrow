8fcda314aae431ba30b61411082cf735598652b5
ujjwalofficial102 <ujjwalmishra102@gmail.com>
2025-04-20 00:10:17 +0530

    frontend pages and components created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,26 @@
+import { Route, Routes } from "react-router-dom";
+
+import HomePage from "./pages/home/HomePage.jsx";
+import SignUpPage from "./pages/auth/signup/SignUpPage.jsx";
+import LoginPage from "./pages/auth/login/LoginPage.jsx";
+import ProfilePage from "./pages/profile/ProfilePage.jsx";
+import NotificationPage from "./pages/notification/NotificationPage.jsx";
+
+import Sidebar from "./components/common/Sidebar.jsx";
+import RightPanel from "./components/common/RightPanel.jsx";
+
 function App() {
   return (
-    <div>
-      <button className="btn btn-soft">Default</button>
-      <button className="btn btn-soft btn-primary">Primary</button>
-      <button className="btn btn-soft btn-secondary">Secondary</button>
-      <button className="btn btn-soft btn-accent">Accent</button>
-      <button className="btn btn-soft btn-info">Info</button>
-      <button className="btn btn-soft btn-success">Success</button>
-      <button className="btn btn-soft btn-warning">Warning</button>
-      <button className="btn btn-soft btn-error">Error</button>
-      <h1 className="text-2xl">
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Velit,
-        sapiente.
-      </h1>
+    <div className="flex max-w-6xl mx-auto">
+      <Sidebar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/notifications" element={<NotificationPage />} />
+        <Route path="/profile/:username" element={<ProfilePage />} />
+      </Routes>
+      <RightPanel />
     </div>
   );
 }
---
+++ frontend/src/pages/home/CreatePost.jsx
@@ -0,0 +1,91 @@
+import { CiImageOn } from "react-icons/ci";
+import { BsEmojiSmileFill } from "react-icons/bs";
+import { useEffect, useRef, useState } from "react";
+import { IoCloseSharp } from "react-icons/io5";
+
+const CreatePost = () => {
+  const [text, setText] = useState("");
+  const [img, setImg] = useState(null);
+
+  const imgRef = useRef(null);
+
+  const isPending = false;
+  const isError = false;
+
+  const data = {
+    profileImg: "/avatars/boy1.png",
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    alert("Post created successfully");
+  };
+
+  const handleImgChange = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.readAsDataURL(file);
+      reader.onload = () => {
+        setImg(reader.result);
+      };
+    }
+  };
+
+  return (
+    <div className="flex p-4 items-start gap-4 border-b border-gray-700">
+      <div className="avatar">
+        <div className="w-8 rounded-full">
+          <img src={data.profileImg || "/avatar-placeholder.png"} />
+        </div>
+      </div>
+      <form className="flex flex-col gap-2 w-full" onSubmit={handleSubmit}>
+        <textarea
+          className="textarea bg-[#101010] w-full pt-1 text-lg resize-none border-none focus:outline-none  border-gray-900"
+          placeholder="What is happening?!"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        />
+        {img && (
+          <div className="relative w-72 mx-auto">
+            <IoCloseSharp
+              className="absolute top-0 right-0 text-white bg-gray-800 rounded-full w-5 h-5 cursor-pointer"
+              onClick={() => {
+                setImg(null);
+                imgRef.current.value = null;
+              }}
+            />
+            <img
+              src={img}
+              className="w-full mx-auto h-72 object-contain rounded"
+            />
+          </div>
+        )}
+
+        <div className="flex justify-between  py-2">
+          <div className="flex gap-1 items-center">
+            <CiImageOn
+              className="fill-secondary w-6 h-6 cursor-pointer"
+              onClick={() => imgRef.current.click()}
+            />
+            <BsEmojiSmileFill className="fill-yellow-500 w-5 h-5 cursor-pointer" />
+          </div>
+          <input
+            type="file"
+            accept="image/*"
+            hidden
+            ref={imgRef}
+            onChange={handleImgChange}
+          />
+          <button className="btn btn-primary rounded-full btn-sm text-white px-4">
+            {isPending ? "Posting..." : "Post"}
+          </button>
+        </div>
+        {isError && (
+          <div className="text-red-600 text-sm">Something went wrong</div>
+        )}
+      </form>
+    </div>
+  );
+};
+export default CreatePost;
