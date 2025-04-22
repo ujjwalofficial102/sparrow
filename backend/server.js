38aa5e1917b13276772b4be9f9770fd4291e014a
ujjwalofficial102 <ujjwalmishra102@gmail.com>
2025-04-22 23:27:16 +0530

    increased payload limit

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(cookieParser());
 
 dotenv.config();
-app.use(express.json());
+app.use(express.json({ limit: "5mb" }));
 app.use(express.urlencoded({ extended: true }));
 
 cloudinary.config({
