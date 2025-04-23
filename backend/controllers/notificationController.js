8f90b33a90d1e0359d1de31b87cdf3974d053b04
ujjwalofficial102 <ujjwalmishra102@gmail.com>
2025-04-24 00:52:54 +0530

    minor changes

--- backend/controllers/notificationController.js
+++ backend/controllers/notificationController.js
@@ -4,10 +4,12 @@ export const getNotifications = async (req, res) => {
   try {
     const userId = req.user._id;
 
-    const notifications = await Notification.find({ to: userId }).populate({
-      path: "from",
-      select: "username profileImg",
-    });
+    const notifications = await Notification.find({ to: userId })
+      .populate({
+        path: "from",
+        select: "username profileImg",
+      })
+      .sort({ createdAt: -1 });
     await Notification.updateMany({ to: userId }, { read: true });
 
     res.status(200).json({
@@ -30,7 +32,7 @@ export const deleteNotifications = async (req, res) => {
     await Notification.deleteMany({ to: userId });
     res.status(200).json({
       success: true,
-      message: "Notification deleted successfully",
+      message: "Notifications deleted successfully",
     });
   } catch (error) {
     console.log("Error in deleteNotifications: ", error);
