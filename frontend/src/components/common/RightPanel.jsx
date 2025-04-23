64be285f09ad08c4116fd1d9bd2ba425d8be7c6f
ujjwalofficial102 <ujjwalmishra102@gmail.com>
2025-04-23 22:17:47 +0530

    follow functionality added

--- frontend/src/components/common/RightPanel.jsx
+++ frontend/src/components/common/RightPanel.jsx
@@ -1,9 +1,11 @@
 import { Link } from "react-router-dom";
 import RightPanelSkeleton from "../skeletons/RightPanelSkeleton";
 import { useQuery } from "@tanstack/react-query";
+import LoadingSpinner from "./LoadingSpinner";
+import { useFollow } from "../../hooks/useFollow";
 
 const RightPanel = () => {
-  const { data: suggestedUser, isLoading } = useQuery({
+  const { data: suggestedUsers, isLoading } = useQuery({
     queryKey: ["suggestedUser"],
     queryFn: async () => {
       try {
@@ -18,7 +20,11 @@ const RightPanel = () => {
       }
     },
   });
-  if (suggestedUser?.length === 0) return <div className="md:w-64 w-0"></div>;
+
+  const { follow, isPending } = useFollow();
+
+  if (!isLoading && suggestedUsers?.length === 0)
+    return <div className="md:w-64 w-0"></div>;
 
   return (
     <div className="hidden lg:block my-4 mx-2">
@@ -35,7 +41,7 @@ const RightPanel = () => {
             </>
           )}
           {!isLoading &&
-            suggestedUser?.map((user) => (
+            suggestedUsers?.map((user) => (
               <Link
                 to={`/profile/${user.username}`}
                 className="flex items-center justify-between gap-4"
@@ -58,10 +64,15 @@ const RightPanel = () => {
                 </div>
                 <div>
                   <button
+                    disabled={isPending}
                     className="btn bg-white text-black hover:bg-white hover:opacity-90 rounded-full btn-sm"
-                    onClick={(e) => e.preventDefault()}
+                    onClick={(e) => {
+                      e.preventDefault();
+                      follow(user._id);
+                    }}
                   >
                     Follow
+                    {/* {isPending ? <LoadingSpinner size="sm" /> : "Follow"} */}
                   </button>
                 </div>
               </Link>
---
+++ frontend/src/hooks/useFollow.jsx
@@ -0,0 +1,34 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import toast from "react-hot-toast";
+
+export const useFollow = () => {
+  const queryClient = useQueryClient();
+
+  const { mutate: follow, isPending } = useMutation({
+    mutationFn: async (userId) => {
+      try {
+        const res = await fetch(`/api/users/follow/${userId}`, {
+          method: "POST",
+        });
+        const data = await res.json();
+        if (data.success === false) {
+          throw new Error(data.message || "Something went wrong");
+        }
+        return data;
+      } catch (error) {
+        throw new Error(error.message);
+      }
+    },
+    onSuccess: () => {
+      Promise.all([
+        queryClient.invalidateQueries({ queryKey: ["suggestedUser"] }),
+        queryClient.invalidateQueries({ queryKey: ["authUser"] }),
+      ]);
+    },
+    onError: (error) => {
+      toast.error(error.message);
+    },
+  });
+
+  return { follow, isPending };
+};
